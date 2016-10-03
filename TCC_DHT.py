d3b86636e21ec13b094e68d5e63c5e4399fb83e7
WilliamTakeshi <williamtpereira@gmail.com>
2016-10-03 14:24:11 -0300

    Update TCC_DHT.py
    Add some comments

--- TCC_DHT.py
+++ TCC_DHT.py
@@ -1,7 +1,6 @@
-"""<title>Menus, Toolboxes, a full Application</title>
-Most all widgets are used in this example.  A full custom widget
-is included.  A number of connections are used to make the application
-function.
+"""Trabalho de conclusão de curso de graduação apresentado ao Departamento de Engenharia Elétrica da Universidade Federal de São Carlos
+como requisito parcial para a obtenção do título de Bacharel em Engenharia Elétrica.
+Modulo DHT11/DHT22
 """
 import pygame
 import time
@@ -9,32 +8,31 @@ from pygame.locals import *
 
 # the following line is not needed if pgu is installed
 import sys; sys.path.insert(0, "..")
-
-#from Python_DHT_TesteComVariaveisCnsts import *         #Import the function with gives the temperature and humidity CAN BE DELETED IN THE FINAL VERSION
-##from Adafruit_Python_DHT import *                       #Import the function with reads the temperature and humidity
-import Adafruit_DHT
+                       
+import Adafruit_DHT                                     #Import the function with reads the temperature and humidity of DHT11 and DHT22
 from pgu import gui                                     #Import all gui
 from SaveFilesHumidityTemp import SaveFilesHumidityTemp #Import the function with saves the files in .xls
 from ChangeMonthtoPortuguese import *                   #Import the function with changes the month in a Portuguese readable format   
-#from DHT11 import *
+
 
 # --- Global constants ---
-BLACK = (0, 0, 0)    #Colors
+BLACK = (0, 0, 0)                                       #Colors
 WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
  
-SCREEN_WIDTH = 700  
+SCREEN_WIDTH = 700                                      #Display size 
 SCREEN_HEIGHT = 500
 flagStart = 0
-tableHumidityTemp = gui.Table()    #Creating table that will show the humidity and temperature
-select = gui.Select(value=Adafruit_DHT.DHT11) #Creating the selection between the types of sensor, beeing DHT11 the default
-humiditylist = []   #Making the list humidity
-templist = []       #temp
-Date = []           #Date
-Time = []           #and Time globals
+tableHumidityTemp = gui.Table()                         #Creating table that will show the humidity and temperature
+select = gui.Select(value=Adafruit_DHT.DHT11)           #Creating the selection between the types of sensor, beeing DHT11 the default
+humiditylist = []                                       #Creating the list Humidity
+templist = []                                           #Creating the list Temperature
+Date = []                                               #Creating the list Date
+Time = []                                               #Creating the list Time 
+
 # --- Classes ---
-class NewDialog(gui.Dialog): #Creating the dialog when you click File->New
+class NewDialog(gui.Dialog):                            #Creating the dialog when you click File->New
     def __init__(self,**params):
         title = gui.Label("Novo Arquivo...")
         
@@ -59,7 +57,7 @@ class NewDialog(gui.Dialog): #Creating the dialog when you click File->New
         
         gui.Dialog.__init__(self,title,t)
 
-class AboutDialog(gui.Dialog): #Creating the dialog when you click Help->About
+class AboutDialog(gui.Dialog):                          #Creating the dialog when you click Help->About
     def __init__(self,**params):
         title = gui.Label("Sobre o modulo DHT")
         
@@ -86,7 +84,7 @@ class AboutDialog(gui.Dialog): #Creating the dialog when you click Help->About
 ##
         
 
-class HelpDialog(gui.Dialog): #Creating the dialog when you click Help->Help
+class HelpDialog(gui.Dialog):                           #Creating the dialog when you click Help->Help
     def __init__(self,**params):
         title = gui.Label("Ajuda")
         
@@ -108,7 +106,7 @@ class HelpDialog(gui.Dialog): #Creating the dialog when you click Help->Help
 
         gui.Dialog.__init__(self,title,doc)
         
-class QuitDialog(gui.Dialog): #Creating the dialog when you click File->Quit
+class QuitDialog(gui.Dialog):                          #Creating the dialog when you click File->Quit
     def __init__(self,**params):
         title = gui.Label("Sair")
         
@@ -128,7 +126,7 @@ class QuitDialog(gui.Dialog): #Creating the dialog when you click File->Quit
         
         gui.Dialog.__init__(self,title,t)
 
-class OpenDialog(gui.Dialog):  #Creating the dialog when you click File->Open
+class OpenDialog(gui.Dialog):                           #Creating the dialog when you click File->Open
     def __init__(self,**params):
         title = gui.Label("Abrir")
         
@@ -151,7 +149,7 @@ class OpenDialog(gui.Dialog):  #Creating the dialog when you click File->Open
         
         gui.Dialog.__init__(self,title,t)
 
-class SaveDialog(gui.Dialog): #Creating the dialog when you click File->SaveAs
+class SaveDialog(gui.Dialog):                           #Creating the dialog when you click File->SaveAs
     def __init__(self,**params):
         title = gui.Label("Salvar como...")
         
@@ -174,48 +172,38 @@ class SaveDialog(gui.Dialog): #Creating the dialog when you click File->SaveAs
         
         gui.Dialog.__init__(self,title,t)
         
-class StartGameButton(gui.Button): #The button that starts reading the values in the sensor
+class StartGameButton(gui.Button):                      #The button that starts reading the values in the sensor
     def __init__(self,**params):
         params['value'] = 'Comecar leitura'
         gui.Button.__init__(self,**params)
         self.connect(gui.CLICK,WriteTableHomeScreen)
 
-class ClearButton(gui.Button): #The button just to test, delete before final product
-    def __init__(self,**params):
-        params['value'] = 'Limpar'
-        gui.Button.__init__(self,**params)
-        self.connect(gui.CLICK,tableHumidityTemp.clear) 
 
-class HomeScreen(gui.Desktop):  #Create the container of the HomeScreen and add all the buttons and tables
+class HomeScreen(gui.Desktop):                          #Create the container of the HomeScreen and add all the buttons and tables
     def __init__(self,**params):
         gui.Desktop.__init__(self,**params)
         
         self.connect(gui.QUIT,self.quit,None)
         
-        c = gui.Container(width=SCREEN_WIDTH,height=SCREEN_HEIGHT) #Create the screen with the WIDTH and HEIGHT specified before
+        c = gui.Container(width=SCREEN_WIDTH,height=SCREEN_HEIGHT)  #Create the screen with the WIDTH and HEIGHT specified before
 
 
-        box = gui.ScrollArea(tableHumidityTemp,680,240)            #Create the box where all the data will be shown up
+        box = gui.ScrollArea(tableHumidityTemp,680,240)             #Create the box where all the data will be shown up
         c.add(box,10,120)
 
         e = StartGameButton() 
 
         c.add(e,20,40) ##Add the StartGameBtton
 
-########################################################################################        
-        e = ClearButton()
-
-        c.add(e,300,40) ##Add the ClearButton
-##############################################################################################
         
-        select.add("DHT11",Adafruit_DHT.DHT11)                               #Sensors that can be choosen IF YOU NEED MORE YOU WILL NEED TO ADD IN THE Adafruit FUNCTION
+        select.add("DHT11",Adafruit_DHT.DHT11)                      #Sensors that can be choosen
         select.add("DHT22",Adafruit_DHT.DHT22)
-        c.add(select,190,40)                                      #Create the select box that will be used to choose the sensor
+        c.add(select,190,40)                                        #Create the select box that will be used to choose the sensor
 
         
-        self.fname = 'untitled.xls'                               #Name of the .xls file
+        self.fname = 'untitled.xls'                                 #Name of the .xls file
         
-        self.new_d = NewDialog()                                  #Opening the file->new, open, save and qui dialong in the HomeScreen
+        self.new_d = NewDialog()                                    #Opening the file->new, open, save and qui dialong in the HomeScreen
         self.new_d.connect(gui.CHANGE,self.action_new,None)
         self.open_d = OpenDialog()
         self.open_d.connect(gui.CHANGE,self.action_open,None)
@@ -224,7 +212,7 @@ class HomeScreen(gui.Desktop):  #Create the container of the HomeScreen and add 
         self.quit_d = QuitDialog()
         self.quit_d.connect(QUIT,self.quit,None)
         
-        self.help_d = HelpDialog()                                #Opening the help and about dialong
+        self.help_d = HelpDialog()                                  #Opening the help and about dialong
         self.about_d = AboutDialog()
         
         ##Initializing the Menus, we connect to a number of Dialog.open methods for each of the dialogs.
@@ -245,7 +233,7 @@ class HomeScreen(gui.Desktop):  #Create the container of the HomeScreen and add 
         
         self.widget = c
         
-    def action_new(self,value):
+    def action_new(self,value):                                     #Setting what the actions New, Save, Saveas and Open do
         self.new_d.close()
         self.fname = self.new_d.value['fname'].value
         global tableHumidityTemp
@@ -254,40 +242,40 @@ class HomeScreen(gui.Desktop):  #Create the container of the HomeScreen and add 
         tableHumidityTemp.clear()
         humiditylist = []
         templist = []
+        
     def action_save(self,value):
-        SaveFilesHumidityTemp(self.fname, "DHT", humiditylist, templist, Date, Time)
+        SaveFilesHumidityTemp(self.fname, "DHT", humiditylist, templist, Date, Time)  
         
     def action_saveas(self,value):
         self.save_d.close()
         self.fname = self.save_d.value['fname'].value
         SaveFilesHumidityTemp(self.fname, "DHT", humiditylist, templist, Date, Time)
+        
     def action_open(self,value):
         self.open_d.close()
         self.fname = self.open_d.value['fname']
         self.painter.surface = pygame.image.load(self.fname)
         self.painter.repaint()
         
-def WriteTableHomeScreen(): #Function that Reads the sensor and prints in the table
+def WriteTableHomeScreen():                                         #Function that Reads the sensor and prints in the table
     global flagStart
     global c
-    if flagStart:
-        flagStart = 0
-        
+    if flagStart:                                                   #The flagStart tells whether the program is or is not collecting data
+        flagStart = 0 
     else:
         flagStart = 1
-    print(flagStart)
+        
     while flagStart:
-##        humidity, temp = ReadHumidityTemp(select.value,2) #(sensor,pin) ex: (11,9)
-        humidity, temp = Adafruit_DHT.read_retry(select.value,2)
+        humidity, temp = Adafruit_DHT.read_retry(select.value,2)    #Read the sensor
         print('humidity{0:0.1f} temp{0:0.1f}'.format(humidity, temp))
-        humiditylist.append(humidity)
+        humiditylist.append(humidity)                               #Add to the humidity and temperature lists
         templist.append(temp)
-        DateTime = time.localtime()
+        DateTime = time.localtime()                                 #Reads the time and add in the date and time lists
         Datestring = str(DateTime[2])+"/"+str(DateTime[1])+"/"+str(DateTime[0]) #Putting the date in a string to make easier to put n the excel table
         Date.append(Datestring)
         Timestring = str(DateTime[3])+":"+str(DateTime[4])+":"+str(DateTime[5]) #Putting the time in a string to make easier to put n the excel table
         Time.append(Timestring)
-        DateTime = ChangeMonthtoPortuguese(DateTime)
+        DateTime = ChangeMonthtoPortuguese(DateTime)                #Just change to portuguese to print in the screen
         tableHumidityTemp.tr()
         tableHumidityTemp.td(gui.Label(str((DateTime[9],
                                             "Umidade: ",humidity,"Temperatura: ",temp))))
